28466ba6fa7d705263743cacd761ea56e7dcfff9
sethg <sethg@geographika.co.uk>
2019-02-22 18:09:45 +0100

    Add comments to code

--- create_sample_map.py
+++ create_sample_map.py
@@ -41,6 +41,8 @@ CLASS
 END
 """
 
+# colours of the rainbow
+
 colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', 
           '#0000FF', '#4B0082', '#9400D3']
 
--- mappyfile_colors.py
+++ mappyfile_colors.py
@@ -4,7 +4,7 @@ from lark.visitors import v_args
 import webcolors
 from lark.lexer import Token
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 class ConversionType:
@@ -14,6 +14,11 @@ class ConversionType:
 
 
 class ColorToken(Token):
+    """
+    A token subclass with an additional comment attribute
+    to store the colour name so it can be accessed
+    by the transformer
+    """
     __slots__ = 'comment'
 
 
@@ -27,6 +32,10 @@ orig_function = CommentsTransformer._save_attr_comments
 
 @v_args(tree=True)
 def attr_comments_override(self, tree):
+    """
+    Create a comment with the colour name to the token
+    and append to any existing comment
+    """
     d = orig_function(self, tree)
     if "__tokens__" in d:
         for t in d["__tokens__"]:
@@ -39,7 +48,10 @@ CommentsTransformer.attr = attr_comments_override
 
 
 class ColoursTransformer(MapfileTransformer):
-
+    """
+    A custom transformer that can convert from RGB to HEX colour
+    formats, and add in the colour name as a comment
+    """
     def __init__(self, include_position=False, include_comments=False,
                  conversion_type=ConversionType.TO_RGB):
 
@@ -47,7 +59,10 @@ class ColoursTransformer(MapfileTransformer):
         super(ColoursTransformer, self).__init__(include_position, include_comments)
 
     def rgb(self, t):
-
+        """
+        Convert rgb values to hex if appropriate, if not
+        return the standard transformation
+        """
         if self.conversion_type == ConversionType.TO_RGB:
             return super(ColoursTransformer, self).rgb(t)
         else:
@@ -57,8 +72,21 @@ class ColoursTransformer(MapfileTransformer):
             converted_token.comment = named_colour
             return converted_token
 
-    def colorrange(self, t):
+    def hexcolor(self, t):
+        """
+        Convert hex values to rgb if appropriate, if not
+        return the standard transformation
+        """
+        if self.conversion_type == ConversionType.TO_RGB:
+            r, g, b = webcolors.hex_to_rgb(self.clean_string(t[0]))
+            return Token("RGB", [r, g, b])
+        else:
+            return super(ColoursTransformer, self).hexcolor(t)
 
+    def colorrange(self, t):
+        """
+        Convert a rgb colorange to a hex colorrange
+        """
         if self.conversion_type == ConversionType.TO_RGB:
             return super(ColoursTransformer, self).colorrange(t)
         else:
@@ -73,26 +101,24 @@ class ColoursTransformer(MapfileTransformer):
             return [hex_token1, hex_token2]
 
     def hexcolorrange(self, t):
-
+        """
+        Convert a hex colorange to a rgb colorrange
+        """
         if self.conversion_type == ConversionType.TO_RGB:
             r1, g1, b1 = t[0].value
             r2, g2, b2 = t[1].value
+            # TODO why can't 2 RGB values be returned here?
             return [Token("SIGNED_INT", r1), Token("SIGNED_INT", g1), Token("SIGNED_INT", b1),
                     Token("SIGNED_INT", r2), Token("SIGNED_INT", g2), Token("SIGNED_INT", b2)]
         else:
             return super(ColoursTransformer, self).hexcolorrange(t)
 
-    def hexcolor(self, t):
-
-        if self.conversion_type == ConversionType.TO_RGB:
-            r, g, b = webcolors.hex_to_rgb(self.clean_string(t[0]))
-            return Token("RGB", [r, g, b])
-        else:
-            return super(ColoursTransformer, self).hexcolor(t)
-
 
 def colours_transform(s, conversion_type=ConversionType.NO_CONVERSION, include_comments=False):
     p = Parser(include_comments=include_comments)
+    """
+    Parse the string with a custom colours transformer
+    """
     ast = p.parse(s)
 
     m = MapfileToDict(include_comments=include_comments,
