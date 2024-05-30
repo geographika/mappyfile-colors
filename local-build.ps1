4e57e736a19475a2e5d543567ed85c3a5ab1270a
sethg <sethg@geographika.co.uk>
2024-05-30 17:58:30 +0200

    Build wheels

---
+++ local-build.ps1
@@ -0,0 +1,7 @@
+C:\Python310\python -m venv C:\VirtualEnvs\mappyfile-colors
+C:\VirtualEnvs\mappyfile-colors\Scripts\activate.ps1
+cd D:\GitHub\mappyfile-colors
+pip install -e .
+pip install -r requirements-dev.txt
+
+pytest
